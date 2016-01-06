618bac73aac5503f84c437ecf8fb14bb9406e4fb
baumannc <baumannc@hochschule-trier.de>
2016-01-06 14:44:23 +0100

    Update 2016-01-06T13:44:17.396Z

--- angular-threenorama.js
+++ angular-threenorama.js
@@ -19,9 +19,7 @@
         end: '=',
         xGeneric: '=',
         yGeneric: '=',
-        panoramaZoom: '=',
-        picWidth: '=',
-        picHeight: '='
+        panoramaZoom: '='
       },
       link: function(scope, element, attrs, ctrl, transcludeFn) {
         var canvas, getMousePos, mouseDown, mouseDrag, mouseMove, mouseUp, resize, sourceChanged, updateAngles, zoomChanged;
@@ -42,12 +40,6 @@
             tilt: 0,
             pan: 0
           };
-        }
-        if (scope.picWidth == null) {
-          scope.picWidth = 0.18728789;
-        }
-        if (scope.picHeight == null) {
-          scope.picHeight = -0.3221746;
         }
         if (scope.panoramaZoom == null) {
           scope.panoramaZoom = document.getElementById("zoom").value;
@@ -125,91 +117,93 @@
           return ngThreenorama.mouseDrag(new THREE.Vector2(xy[0], xy[1]));
         };
         updateAngles = function(event) {
-          var cols, correction360, endX, endY, focalDistance, i, maxFocal, minFocal, minZoomHeight, minZoomWidth, panAngle, pictureCoverage, rows, startX, startY, tiltAngle, xDelta, yDelta;
-          scope.xGeneric = {
-            xMin: document.getElementById("xMin").value,
-            xMax: document.getElementById("xMax").value
-          };
-          scope.yGeneric = {
-            yMin: document.getElementById("yMin").value,
-            yMax: document.getElementById("yMax").value
-          };
-          minFocal = 0.0;
-          maxFocal = 0.0;
-          minFocal = document.getElementById("minFocal").value;
-          maxFocal = document.getElementById("maxFocal").value;
-          focalDistance = 1 * minFocal + ((maxFocal - minFocal) * (document.getElementById("zoom").value / 100));
-          panAngle = Math.atan(document.getElementById("sensorWidth").value / (2 * focalDistance)) * (180 / Math.PI);
-          tiltAngle = Math.atan(document.getElementById("sensorHeight").value / (2 * focalDistance)) * (180 / Math.PI);
-          minZoomWidth = Math.atan(document.getElementById("sensorWidth").value / (2 * minFocal)) * (180 / Math.PI);
-          minZoomHeight = Math.atan(document.getElementById("sensorHeight").value / (2 * minFocal)) * (180 / Math.PI);
-          pictureCoverage = 1 - document.getElementById("pictureCoverage").value / 100;
-          xDelta = panAngle * pictureCoverage / 360 * scope.xGeneric.xMin + panAngle * pictureCoverage / 360 * scope.xGeneric.xMax;
-          yDelta = tiltAngle * pictureCoverage / 180 * scope.yGeneric.yMin + tiltAngle * pictureCoverage / 180 * scope.yGeneric.yMax;
-          startX = ngThreenorama.ngtAnchors.startPan - minZoomWidth / 2;
-          if (startX < 0) {
-            startX = 360 + startX;
-          }
-          startY = ngThreenorama.ngtAnchors.startTilt - minZoomHeight / 2;
-          endX = ngThreenorama.ngtAnchors.endPan - minZoomWidth / 2;
-          if (endX < 0) {
-            endX = 360 + endX;
-          }
-          endY = ngThreenorama.ngtAnchors.endTilt - minZoomHeight / 2;
-          scope.start.pan = (startX + panAngle / 2) % 360;
-          scope.start.tilt = startY + tiltAngle / 2;
-          scope.end.pan = (endX - panAngle / 2) % 360;
-          scope.end.tilt = endY - tiltAngle / 2;
-          if ((ngThreenorama.ngtAnchors.startPan + panAngle >= ngThreenorama.ngtAnchors.endPan && ngThreenorama.ngtAnchors.startPan < ngThreenorama.ngtAnchors.endPan) || (ngThreenorama.ngtAnchors.endPan + panAngle >= ngThreenorama.ngtAnchors.startPan && ngThreenorama.ngtAnchors.endPan < ngThreenorama.ngtAnchors.startPan)) {
-            scope.end.pan = scope.start.pan;
-          }
-          if ((ngThreenorama.ngtAnchors.startTilt + tiltAngle >= ngThreenorama.ngtAnchors.endTilt && ngThreenorama.ngtAnchors.startTilt < ngThreenorama.ngtAnchors.endTilt) || (ngThreenorama.ngtAnchors.endTilt + tiltAngle >= ngThreenorama.ngtAnchors.startTilt && ngThreenorama.ngtAnchors.endTilt < ngThreenorama.ngtAnchors.startTilt)) {
-            scope.end.tilt = scope.start.tilt;
-          }
-          scope.start.pan = scope.start.pan / 360 * scope.xGeneric.xMin + scope.start.pan / 360 * scope.xGeneric.xMax;
-          scope.start.tilt = scope.start.tilt / 180 * scope.yGeneric.yMin + scope.start.tilt / 180 * scope.yGeneric.yMax;
-          if (scope.start.tilt > 0) {
-            scope.start.tilt = 0;
-            ngThreenorama.ngtAnchors.tiltOutOfLimit = true;
-          } else {
-            ngThreenorama.ngtAnchors.tiltOutOfLimit = false;
-          }
-          scope.end.pan = scope.end.pan / 360 * scope.xGeneric.xMin + scope.end.pan / 360 * scope.xGeneric.xMax;
-          scope.end.tilt = scope.end.tilt / 180 * scope.yGeneric.yMin + scope.end.tilt / 180 * scope.yGeneric.yMax;
-          if (scope.end.tilt > 0) {
-            scope.end.tilt = 0;
-          }
-          i = scope.start.pan;
-          correction360 = 0;
-          if (i > scope.end.pan) {
-            correction360 = 1;
-          }
-          cols = 1;
-          while (i + xDelta < scope.end.pan + correction360) {
-            cols++;
-            i += xDelta;
-          }
-          i = scope.start.tilt;
-          if (scope.start.tilt > 0) {
-            i *= -1;
-          }
-          rows = 1;
-          while (i + yDelta > scope.end.tilt) {
-            rows++;
-            i += yDelta;
-          }
-          document.getElementById("xStart").textContent = scope.start.pan;
-          document.getElementById("yStart").textContent = scope.start.tilt;
-          document.getElementById("xEnd").textContent = scope.end.pan;
-          document.getElementById("yEnd").textContent = scope.end.tilt;
-          document.getElementById("xDelta").textContent = xDelta;
-          document.getElementById("yDelta").textContent = yDelta;
-          document.getElementById("zoomGeneric").textContent = document.getElementById("zoom").value / 100 * document.getElementById("minZoom").value + document.getElementById("zoom").value / 100 * document.getElementById("maxZoom").value;
-          document.getElementById("numOfPicutres").textContent = rows + " x " + cols + " = " + (rows * cols);
-          if (ngThreenorama.ngtAnchors.tiltOutOfLimit) {
-            document.getElementById("numOfPicutres").textContent += ", Warning: Start Tilt is out of Range!";
-          }
-          return ngThreenorama.outOfRangeColorChange();
+          return scope.$apply(function() {
+            var cols, correction360, endX, endY, focalDistance, i, maxFocal, minFocal, minZoomHeight, minZoomWidth, panAngle, pictureCoverage, rows, startX, startY, tiltAngle, xDelta, yDelta;
+            scope.xGeneric = {
+              xMin: document.getElementById("xMin").value,
+              xMax: document.getElementById("xMax").value
+            };
+            scope.yGeneric = {
+              yMin: document.getElementById("yMin").value,
+              yMax: document.getElementById("yMax").value
+            };
+            minFocal = 0.0;
+            maxFocal = 0.0;
+            minFocal = document.getElementById("minFocal").value;
+            maxFocal = document.getElementById("maxFocal").value;
+            focalDistance = 1 * minFocal + ((maxFocal - minFocal) * (document.getElementById("zoom").value / 100));
+            panAngle = Math.atan(document.getElementById("sensorWidth").value / (2 * focalDistance)) * (180 / Math.PI);
+            tiltAngle = Math.atan(document.getElementById("sensorHeight").value / (2 * focalDistance)) * (180 / Math.PI);
+            minZoomWidth = Math.atan(document.getElementById("sensorWidth").value / (2 * minFocal)) * (180 / Math.PI);
+            minZoomHeight = Math.atan(document.getElementById("sensorHeight").value / (2 * minFocal)) * (180 / Math.PI);
+            pictureCoverage = 1 - document.getElementById("pictureCoverage").value / 100;
+            xDelta = panAngle * pictureCoverage / 360 * scope.xGeneric.xMin + panAngle * pictureCoverage / 360 * scope.xGeneric.xMax;
+            yDelta = tiltAngle * pictureCoverage / 180 * scope.yGeneric.yMin + tiltAngle * pictureCoverage / 180 * scope.yGeneric.yMax;
+            startX = ngThreenorama.ngtAnchors.startPan - minZoomWidth / 2;
+            if (startX < 0) {
+              startX = 360 + startX;
+            }
+            startY = ngThreenorama.ngtAnchors.startTilt - minZoomHeight / 2;
+            endX = ngThreenorama.ngtAnchors.endPan - minZoomWidth / 2;
+            if (endX < 0) {
+              endX = 360 + endX;
+            }
+            endY = ngThreenorama.ngtAnchors.endTilt - minZoomHeight / 2;
+            scope.start.pan = (startX + panAngle / 2) % 360;
+            scope.start.tilt = startY + tiltAngle / 2;
+            scope.end.pan = (endX - panAngle / 2) % 360;
+            scope.end.tilt = endY - tiltAngle / 2;
+            if ((ngThreenorama.ngtAnchors.startPan + panAngle >= ngThreenorama.ngtAnchors.endPan && ngThreenorama.ngtAnchors.startPan < ngThreenorama.ngtAnchors.endPan) || (ngThreenorama.ngtAnchors.endPan + panAngle >= ngThreenorama.ngtAnchors.startPan && ngThreenorama.ngtAnchors.endPan < ngThreenorama.ngtAnchors.startPan)) {
+              scope.end.pan = scope.start.pan;
+            }
+            if ((ngThreenorama.ngtAnchors.startTilt + tiltAngle >= ngThreenorama.ngtAnchors.endTilt && ngThreenorama.ngtAnchors.startTilt < ngThreenorama.ngtAnchors.endTilt) || (ngThreenorama.ngtAnchors.endTilt + tiltAngle >= ngThreenorama.ngtAnchors.startTilt && ngThreenorama.ngtAnchors.endTilt < ngThreenorama.ngtAnchors.startTilt)) {
+              scope.end.tilt = scope.start.tilt;
+            }
+            scope.start.pan = scope.start.pan / 360 * scope.xGeneric.xMin + scope.start.pan / 360 * scope.xGeneric.xMax;
+            scope.start.tilt = scope.start.tilt / 180 * scope.yGeneric.yMin + scope.start.tilt / 180 * scope.yGeneric.yMax;
+            if (scope.start.tilt > 0) {
+              scope.start.tilt = 0;
+              ngThreenorama.ngtAnchors.tiltOutOfLimit = true;
+            } else {
+              ngThreenorama.ngtAnchors.tiltOutOfLimit = false;
+            }
+            scope.end.pan = scope.end.pan / 360 * scope.xGeneric.xMin + scope.end.pan / 360 * scope.xGeneric.xMax;
+            scope.end.tilt = scope.end.tilt / 180 * scope.yGeneric.yMin + scope.end.tilt / 180 * scope.yGeneric.yMax;
+            if (scope.end.tilt > 0) {
+              scope.end.tilt = 0;
+            }
+            i = scope.start.pan;
+            correction360 = 0;
+            if (i > scope.end.pan) {
+              correction360 = 1;
+            }
+            cols = 1;
+            while (i + xDelta < scope.end.pan + correction360) {
+              cols++;
+              i += xDelta;
+            }
+            i = scope.start.tilt;
+            if (scope.start.tilt > 0) {
+              i *= -1;
+            }
+            rows = 1;
+            while (i + yDelta > scope.end.tilt) {
+              rows++;
+              i += yDelta;
+            }
+            document.getElementById("xStart").textContent = scope.start.pan;
+            document.getElementById("yStart").textContent = scope.start.tilt;
+            document.getElementById("xEnd").textContent = scope.end.pan;
+            document.getElementById("yEnd").textContent = scope.end.tilt;
+            document.getElementById("xDelta").textContent = xDelta;
+            document.getElementById("yDelta").textContent = yDelta;
+            document.getElementById("zoomGeneric").textContent = document.getElementById("zoom").value / 100 * document.getElementById("minZoom").value + document.getElementById("zoom").value / 100 * document.getElementById("maxZoom").value;
+            document.getElementById("numOfPicutres").textContent = rows + " x " + cols + " = " + (rows * cols);
+            if (ngThreenorama.ngtAnchors.tiltOutOfLimit) {
+              document.getElementById("numOfPicutres").textContent += ", Warning: Start Tilt is out of Range!";
+            }
+            return ngThreenorama.outOfRangeColorChange();
+          });
         };
         canvas = angular.element('<canvas>')[0];
         ngThreenorama.init();
@@ -220,12 +214,10 @@
         element[0].onmousemove = mouseMove;
         element[0].onmousedrag = mouseDrag;
         ngThreenorama.onChangeRect = function() {
-          return scope.$apply(function() {
-            return updateAngles();
-          });
+          return updateAngles();
         };
-        scope.$watch('zoom', zoomChanged);
-        scope.$watch('src', sourceChanged);
+        scope.$watch('zoom', zoomChanged, true);
+        scope.$watch('src', sourceChanged, true);
         $window.onresize = resize;
         return transcludeFn(scope, function(clonedTranscludeTemplate) {
           return element.append(clonedTranscludeTemplate);
