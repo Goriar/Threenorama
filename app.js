0393fe84412bd66ea24ca45f246399c64e9f4176
baumannc <baumannc@hochschule-trier.de>
2016-01-15 14:37:03 +0100

    Update 2016-01-15T13:36:40.953Z

---
+++ app.js
@@ -0,0 +1,67 @@
+(function() {
+  var threenoramaApp;
+
+  threenoramaApp = angular.module('ThreenoramaApp', ['ngThreenorama']);
+
+  threenoramaApp.controller('ThreenoramaCntrl', function($scope, $http) {
+    $scope.pictureUploaded = function() {
+      var input, reader;
+      input = document.getElementById("picUpload");
+      reader = new FileReader();
+      reader.onload = (function(theFile) {
+        return function(e) {
+          return $scope.$apply(function() {
+            return $scope.url = e.target.result;
+          });
+        };
+      })(input.files[0]);
+      return reader.readAsDataURL(input.files[0]);
+    };
+    $scope.setSource1 = function() {
+      return $scope.url = 'img/pano.jpg';
+    };
+    $scope.setSource2 = function() {
+      var input;
+      input = document.getElementById("picUpload");
+      return input.click();
+    };
+    $scope.pictureOverlap = function(value) {
+      return $scope.overlap = value;
+    };
+    return $scope.createRecorderjob = function(cam) {
+      var errorCallback, successCallback, xDelta, xEnd, xStart, yDelta, yEnd, yStart, zoom;
+      successCallback = function(response) {
+        return alert("Success!");
+      };
+      errorCallback = function(response) {
+        return alert("Failure!");
+      };
+      xStart = document.getElementById("xStart").textContent;
+      yStart = document.getElementById("yStart").textContent;
+      xEnd = document.getElementById("xEnd").textContent;
+      yEnd = document.getElementById("yEnd").textContent;
+      xDelta = document.getElementById("xDelta").textContent;
+      yDelta = document.getElementById("yDelta").textContent;
+      zoom = document.getElementById("zoomGeneric").textContent;
+      return $http({
+        method: 'POST',
+        url: '/recorderjob',
+        data: {
+          name: 'Test',
+          active: true,
+          camera: cam,
+          panorama: {
+            startX: xStart,
+            startY: yStart,
+            endX: xEnd,
+            endY: yEnd,
+            deltaX: xDelta,
+            deltaY: yDelta,
+            zoom: zoom
+          }
+        }
+      }).then(successCallback, errorCallback);
+    };
+  });
+
+}).call(this);
