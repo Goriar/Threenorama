8462f63b6cae3ee866b3aa06ff3c3f08f564fe2c
baumannc <baumannc@hochschule-trier.de>
2016-01-18 01:52:18 +0100

    Update 2016-01-18T00:52:07.026Z

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@
     $scope.pictureOverlap = function(value) {
       return $scope.overlap = value;
     };
-    return $scope.createRecorderjob = function(cam) {
+    $scope.createRecorderjob = function(cam) {
       var errorCallback, successCallback, tmp, xDelta, xEnd, xStart, yDelta, yEnd, yStart, zoom;
       successCallback = function(response) {
         return alert("Success!");
@@ -55,7 +55,50 @@
         method: 'POST',
         url: '/recorderjob',
         data: {
-          name: 'Test',
+          name: 'Threenorama',
+          active: true,
+          camera: {
+            id: cam
+          },
+          panorama: {
+            startX: xStart,
+            startY: yStart,
+            endX: xEnd,
+            endY: yEnd,
+            deltaX: xDelta,
+            deltaY: yDelta,
+            zoom: zoom
+          }
+        }
+      }).then(successCallback, errorCallback);
+    };
+    return $scope.create360Pano = function(cam) {
+      var errorCallback, focalDistance, maxFocal, minFocal, panAngle, successCallback, tiltAngle, xDelta, xEnd, xStart, yDelta, yEnd, yStart, zoom;
+      successCallback = function(response) {
+        return alert("Success!");
+      };
+      errorCallback = function(response) {
+        return alert("Failure!");
+      };
+      xStart = document.getElementById("xMin").value;
+      yStart = document.getElementById("yMin").value;
+      xEnd = document.getElementById("xMax").value;
+      yEnd = {
+        yMax: document.getElementById("yMax").value
+      };
+      minFocal = document.getElementById("minFocal").value;
+      maxFocal = document.getElementById("maxFocal").value;
+      focalDistance = 1 * minFocal + ((maxFocal - minFocal) * (document.getElementById("zoom").value / 100));
+      panAngle = Math.atan(document.getElementById("sensorWidth").value / (2 * focalDistance));
+      tiltAngle = Math.atan(document.getElementById("sensorHeight").value / (2 * focalDistance));
+      xDelta = panAngle * 0.5 / 360 * xStart + panAngle * 0.5 / 360 * xEnd;
+      yDelta = tiltAngle * 0.5 / 180 * yStart + tiltAngle * 0.5 / 180 * yEnd;
+      zoom = document.getElementById("minZoom").value;
+      return $http({
+        method: 'POST',
+        url: '/recorderjob',
+        data: {
+          name: '360Pano',
           active: true,
           camera: {
             id: cam
